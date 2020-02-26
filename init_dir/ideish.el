84bbe62cf14cdba9c67b8399a20b0fc4bdfdc2c4
Gimme Somemore <gimmesomemore@gmail.com>
2020-02-26 14:49:09 +0000

    lsp tuning

--- init_dir/ideish.el
+++ init_dir/ideish.el
@@ -8,17 +8,31 @@
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp)
 
+(use-package ccls
+  :ensure t
+  :hook ((c-mode c++-mode objc-mode) .
+         (lambda () (require 'ccls) (lsp)))
+  :config
+  (setq ccls-executable "ccls")
+  (setq lsp-prefer-flymake nil)
+  (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
+  (lsp-register-client
+   (make-lsp-client
+    :new-connection (lsp-tramp-connection (cons ccls-executable ccls-args))
+    :major-modes '(c-mode c++-mode cuda-mode objc-mode)
+    :server-id 'ccls-remote
+    :multi-root nil
+    :remote? t
+    :notification-handlers
+    (lsp-ht ("$ccls/publishSkippedRanges" #'ccls--publish-skipped-ranges)
+	    ("$ccls/publishSemanticHighlight" #'ccls--publish-semantic-highlight))
+    :initialization-options (lambda () ccls-initialization-options)
+        :library-folders-fn nil))
+)
+
 (use-package lsp-ui :commands lsp-ui-mode :ensure t)
 (use-package company-lsp
   :ensure t
   :commands company-lsp
   :config (push 'company-lsp company-backends)) ;; add company-lsp as a backend
 
-(use-package ccls
-  :ensure t
-  :config
-  (setq ccls-executable "ccls")
-  (setq lsp-prefer-flymake nil)
-  (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
-  :hook ((c-mode c++-mode objc-mode) .
-         (lambda () (require 'ccls) (lsp))))
